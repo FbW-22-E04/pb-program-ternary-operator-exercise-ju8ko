88a8827260e6a638313cb6f81454fd3b699bbc49
ju8ko <julia.x.kobzar@gmail.com>
2022-07-11 16:30:49 +0200

    The exercise is ready

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ if (a + b < 4) {
 } */
 
 /* _________________________________ */
-let a=3;
-let b=6;
+let a = 3;
+let b = 6;
 
-//write your code here 
-console.log(result)
+//write your code here
+console.log(a + b < 4 ? "Below" : "Over");
 
 /* _________________________________ */
-
 
 //Rewrite the code below to use the ternary operator (?:) (you should be able to condense the if-else logic into one line).
 /* if (score > 1337)
@@ -31,15 +30,19 @@ else
 
 /* _________________________________ */
 
-let score = 42;
+let score = 2000;
 let msg = "";
 
-//write your code here 
+//write your code here
 
-console.log(msg)
+console.log(
+  (msg =
+    score > 1337
+      ? "This is a new highscore!"
+      : "You need more points to beat the highscore!")
+);
 
 /* _________________________________ */
-
 
 // Rewrite if..else using multiple ternary operators '?'.
 //For readability, it’s recommended to split the code into multiple lines.
@@ -58,9 +61,18 @@ if (login == 'Employee') {
 
 /* _________________________________ */
 
+//write your code here
 
-//write your code here 
-console.log(message)
+let login = "Director";
+let message;
+login == "Employee"
+  ? (message = "Hello")
+  : login == "Director"
+  ? (message = "Greetings")
+  : login == ""
+  ? (message = "No login")
+  : (message = "");
+
+console.log(message);
 
 /* _________________________________ */
-
